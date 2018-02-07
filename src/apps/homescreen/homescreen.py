9d51805392a231746a3838f6f76783f26dd74d3c
Peter Jensen <tuximail@gmail.com>
2018-02-07 15:33:22 +0100

    apps/homescreen: change bg, added tap animation

--- src/apps/homescreen/homescreen.py
+++ src/apps/homescreen/homescreen.py
@@ -8,6 +8,18 @@ async def swipe_to_rotate():
     swipe = await Swipe(absolute=True)
     ui.display.orientation(degrees(swipe))
 
+async def animate_tap():
+    time_delay = const(40000)
+    draw_delay = const(200000)
+
+    ui.display.text_center(130, 220, 'Tap to unlock', ui.BOLD, ui.GREY, ui.BG)
+
+    sleep = loop.sleep(time_delay)
+    icon = res.load(ui.ICON_CLICK)
+    for t in ui.pulse(draw_delay):
+        fg = ui.blend(ui.GREY, ui.DARK_GREY, t)
+        ui.display.icon(45, 202, icon, fg, ui.BG)
+        yield sleep
 
 async def dim_screen():
     await loop.sleep(5 * 1000000)
@@ -20,21 +32,22 @@ async def dim_screen():
 async def display_homescreen():
     from apps.common import storage
 
+    ui.display.clear()
     if not storage.is_initialized():
-        label = 'Go to trezor.io/start'
-        image = None
+        ui.display.text_center(ui.SCREEN // 2, ui.SCREEN - 20, 'Go to trezor.io/start', ui.BOLD, ui.FG, ui.BG)
+        image = res.load('apps/homescreen/res/bg.toif')
+        ui.display.avatar((ui.SCREEN - 144) // 2, (ui.SCREEN - 144) // 2 - 10, image, ui.WHITE, ui.BLACK)
+        await dim_screen()
     else:
         label = storage.get_label() or 'My TREZOR'
+        ui.display.text_center(ui.SCREEN // 2, 35, label, ui.BOLD, ui.FG, ui.BG)
         image = storage.get_homescreen()
+        if not image:
+            image = res.load('apps/homescreen/res/bg.toif')
+        ui.display.avatar((ui.SCREEN - 144) // 2, (ui.SCREEN - 144) // 2, image, ui.WHITE, ui.BLACK)
+        await animate_tap()
 
-    if not image:
-        image = res.load('apps/homescreen/res/homescreen.toif')
 
-    ui.display.bar(0, 0, ui.SCREEN, ui.SCREEN, ui.BG)
-    ui.display.avatar((ui.SCREEN - 144) // 2, (ui.SCREEN - 144) // 2 - 10, image, ui.WHITE, ui.BLACK)
-    ui.display.text_center(ui.SCREEN // 2, ui.SCREEN - 20, label, ui.BOLD, ui.FG, ui.BG)
-
-    await dim_screen()
 
 
 @unimport
