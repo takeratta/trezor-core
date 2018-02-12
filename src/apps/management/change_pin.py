663acd83ec41e0b2f0db59fa9fde729b901ba019
Peter Jensen <tuximail@gmail.com>
2018-02-12 18:06:11 +0100

    src/apps/management/change_pin: fix pin label

--- src/apps/management/change_pin.py
+++ src/apps/management/change_pin.py
@@ -3,20 +3,20 @@ from trezor import config
 from trezor.pin import pin_to_int, show_pin_timeout
 
 
-async def request_pin(ctx):
+async def request_pin(ctx, code: int = None):
     from trezor.messages.ButtonRequest import ButtonRequest
     from trezor.messages.wire_types import ButtonAck
     from apps.common.request_pin import request_pin
 
     await ctx.call(ButtonRequest(), ButtonAck)
 
-    return await request_pin()
+    return await request_pin(code)
 
 
 async def request_pin_confirm(ctx):
     while True:
-        pin1 = await request_pin(ctx)
-        pin2 = await request_pin(ctx)
+        pin1 = await request_pin(ctx, 2)
+        pin2 = await request_pin(ctx, 3)
         if pin1 == pin2:
             return pin1
         # TODO: display a message and wait
@@ -51,11 +51,9 @@ async def layout_change_pin(ctx, msg):
     from trezor.messages.Success import Success
     from trezor.messages.Failure import Failure
     from trezor.messages.FailureType import PinInvalid
-
     await confirm_change_pin(ctx, msg)
-
     if config.has_pin():
-        curr_pin = await request_pin(ctx)
+        curr_pin = await request_pin(ctx, 1)
     else:
         curr_pin = ''
     if msg.remove:
